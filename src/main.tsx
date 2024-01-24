6d4f8e667c16101712cf6eea6f6f23759f0577e9
tayfunyaltur <tayfunyaltur@gmail.com>
2024-01-25 00:22:14 +0300

    version system added

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import LandingPage from "./pages/LandingPage.tsx";
 import ChatPage from "./pages/ChatPage.tsx";
 import Toastr from "./components/Toastr/index.tsx";
 
-const version = "1.0.0";
+const version = "1.0.1";
 const prevVersion = localStorage.getItem("version");
-if (prevVersion !== version) {
+if (prevVersion?.split(".")[0] !== version.split(".")[0]) {
     localStorage.clear();
     localStorage.setItem("version", version);
     window.location.reload();
