7d057f60e41316b7a785b984b7bc135307f15219
TayfunYalturG <167060549+TayfunYalturG@users.noreply.github.com>
2024-10-25 21:11:13 +0300

    remove all

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    WelcomeMessages: [
-        "Secure your messages with confidence. Textigma brings you powerful encryption and decryption tools, ensuring your communication stays private and protected. Explore a new level of security today.",
-        "Step into a world where your messages are shielded from prying eyes. Textigma empowers you with cutting-edge encryption and decryption features, making your communication secure and confidential. Get started on your journey to privacy.",
-        "Elevate your messaging experience with textigma. Encrypt and decrypt messages seamlessly, knowing that your information is safeguarded. Join us in redefining the way you communicate securely online.",
-        "Embrace the future of secure communication. Textigma invites you to experience a state-of-the-art platform that ensures your messages are for your eyes only. Dive into a world of encryption excellence.",
-        "Greetings from textigma! Unleash the power of encrypted messaging and take control of your online conversations. Welcome to a realm of security and innovation – welcome to textigma!",
-    ],
-};
